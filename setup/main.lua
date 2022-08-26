989ed7e7aab2df46a130509cc496ea2a595dcc23
ajh123 <ajh123@users.noreply.github.com>
2022-08-26 19:48:51 +0100

    first commit

---
+++ setup/main.lua
@@ -0,0 +1,16 @@
+fs.delete("tmp/IPvCC")
+print("Packaging directories...")
+fs.delete("IPvCC_programs")
+fs.makeDir("IPvCC_programs")
+
+fs.move("IPvCC/ifconfig.lua", "IPvCC_programs/ifconfig.lua")
+fs.move("IPvCC/testSend.lua", "IPvCC_programs/testSend.lua")
+fs.move("IPvCC/testRecv.lua", "IPvCC_programs/testRecv.lua")
+
+fs.delete("IPvCC/setup")
+fs.move("IPvCC/IPvCC", "tmp/IPvCC")
+fs.delete("IPvCC")
+fs.move("tmp/IPvCC", "IPvCC")
+fs.delete("tmp/IPvCC")
+
+print("Finished installing IPvCC")
