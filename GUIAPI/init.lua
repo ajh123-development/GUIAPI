6e791d3126335aa73bb6435bb8c7d2329e34b43c
ajh123 <ajh123@users.noreply.github.com>
2022-08-27 09:51:11 +0100

    Clickable button

--- GUIAPI/init.lua
+++ GUIAPI/init.lua
@@ -16,6 +16,7 @@ end
 function GUIAPI.tick()
     for index, value in ipairs(buttons) do
         if GUIAPI.is_instance(value, GUIAPI.Button) then
+            value:check()
             value:draw()
         end
     end
@@ -37,7 +38,7 @@ function GUIAPI.execute()
 
     while running do
         GUIAPI.tick()
-        event = os.pullEvent()
+        event = table.pack(os.pullEvent())
     end
 
     term.redirect(currTerm)
@@ -67,20 +68,48 @@ function GUIAPI.Button:__new(x, y, width, height, func, text)
 end
 
 function GUIAPI.Button:draw()
+    local bg = self.colors.bg
+    if self.isClicked then
+        bg = self.colors.click
+    end
+
     paintutils.drawFilledBox(
         self.x,
         self.y,
         self.x+self.width,
         self.y+self.height,
-        self.colors.bg
+        bg
     )
     local oldColor = term.getTextColor()
-    local x = (self.width/2) + self.x
+    local x = (self.width/2) + self.x - (#self.text/2) + 1
     local y = (self.height/2) + self.y
     term.setCursorPos(x, y)
     term.setTextColor(self.colors.text)
     term.write(self.text)
     term.setTextColor(oldColor)
 end
+
+function GUIAPI.Button:check()
+    if event ~= nil then
+        if #event >= 4 then
+            local e, button, x, y = table.unpack(event)
+            if e == "mouse_click"  then
+                if x > self.x-1 and x < self.x-1 + self.width+2 then
+                    if y > self.y-1 and y < self.y-1 + self.height+2 then
+                        self.isClicked = true
+                    end
+                end
+            end
+            if e == "mouse_up"  then
+                if x > self.x-1 and x < self.x-1 + self.width+2 then
+                    if y > self.y-1 and y < self.y-1 + self.height+2 then
+                        self.isClicked = false
+                    end
+                end
+            end
+        end
+    end
+end
+
 
 return GUIAPI
